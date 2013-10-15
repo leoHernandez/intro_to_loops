ff7f45a17dae2fe9a0d5361a8644b08cd148d329
Raj Ramsaroop <raj@bewareofraj.com>
2013-10-14 23:04:09 -0400

    add practice level view controller

---
+++ forloops/PracticeLevelViewController.h
@@ -0,0 +1,13 @@
+//
+//  PracticeLevelViewController.h
+//  forloops
+//
+//  Created by Rajesh Ramsaroop on 10/14/13.
+//  Copyright (c) 2013 leo Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PracticeLevelViewController : UIViewController
+
+@end
---
+++ forloops/PracticeLevelViewController.m
@@ -0,0 +1,38 @@
+//
+//  PracticeLevelViewController.m
+//  forloops
+//
+//  Created by Rajesh Ramsaroop on 10/14/13.
+//  Copyright (c) 2013 leo Hernandez. All rights reserved.
+//
+
+#import "PracticeLevelViewController.h"
+
+@interface PracticeLevelViewController ()
+
+@end
+
+@implementation PracticeLevelViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
